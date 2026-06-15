cece4afd23e77779d5bbf68bdec4452e349a6e48
LucaSoftwareEngineer <santoro.luca.ing@gmail.com>
2026-06-15 15:52:25 +0200

    initial commit

---
+++ src/main/java/com/betacom/veicoli/ProjectVeicoliApiRestGruppoApplication.java
@@ -0,0 +1,13 @@
+package com.betacom.veicoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectVeicoliApiRestGruppoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectVeicoliApiRestGruppoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betacom/veicoli/ProjectVeicoliApiRestGruppoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betacom.veicoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectVeicoliApiRestGruppoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
