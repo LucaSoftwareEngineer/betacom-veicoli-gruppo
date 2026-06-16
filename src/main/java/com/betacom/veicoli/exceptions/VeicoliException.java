d6d474d10138c7691346ab3e135f9b956aa494de
LucaSoftwareEngineer <santoro.luca.ing@gmail.com>
2026-06-16 15:22:55 +0200

    feat: add VeicoliException

---
+++ src/main/java/com/betacom/veicoli/exceptions/VeicoliException.java
@@ -0,0 +1,11 @@
+package com.betacom.veicoli.exceptions;
+
+public class VeicoliException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public VeicoliException(String msg) {
+		super(msg);
+	}
+	
+}
