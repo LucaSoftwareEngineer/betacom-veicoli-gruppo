2034aa6a75ed4dfc0972587b2345a5458de94717
LucaSoftwareEngineer <santoro.luca.ing@gmail.com>
2026-06-16 11:48:58 +0200

    feat add system message

---
+++ src/main/java/com/betacom/veicoli/models/system/MessageID.java
@@ -0,0 +1,26 @@
+package com.betacom.veicoli.models.system;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class MessageID {
+
+	@Column(length = 4)
+	private String lang;
+	
+	@Column(length = 20)
+	private String cod;
+}
