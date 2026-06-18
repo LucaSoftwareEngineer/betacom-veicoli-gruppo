c3e75090e82f702732727cb316954d09feaf25c8
LucaSoftwareEngineer <santoro.luca.ing@gmail.com>
2026-06-18 15:01:13 +0200

    fix: change type request BiciUpdate in PUT

--- src/main/java/com/betacom/veicoli/controllers/BiciController.java
+++ src/main/java/com/betacom/veicoli/controllers/BiciController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -78,7 +79,7 @@ public class BiciController {
             description = "Questo metodo serve a modificare una bici sul database",
             tags = {"Bici"}
     )
-	@PostMapping("update/{id}")
+	@PutMapping("update/{id}")
 	public ResponseEntity<BiciResponse> update(@PathVariable @Min(value=1, message="l'id deve essere un numero") Integer id, @RequestBody BiciRequest req) throws VeicoliException {
 		return new ResponseEntity<>(biciService.update(req, id), HttpStatus.OK);
 	}
