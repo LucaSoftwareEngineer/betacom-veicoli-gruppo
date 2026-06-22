c8a028147ad39adfbbd03d925474933128f275cf
LucaSoftwareEngineer <santoro.luca.ing@gmail.com>
2026-06-22 17:02:07 +0200

    test: add configuration and classsuite

---
+++ src/test/java/com/betacom/veicoli/SuiteClass.java
@@ -0,0 +1,20 @@
+package com.betacom.veicoli;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+import com.betacom.veicoli.bici.BiciControllerTest;
+import com.betacom.veicoli.macchina.MacchinaControllerTest;
+import com.betacom.veicoli.moto.MotoControllerTest;
+import com.betacom.veicoli.veicolo.VeicoloControllerTest;
+
+@Suite
+@SelectClasses({
+	VeicoloControllerTest.class,
+	MacchinaControllerTest.class,
+	MotoControllerTest.class,
+	BiciControllerTest.class
+})
+public class SuiteClass {
+
+}
---
+++ src/test/java/com/betacom/veicoli/moto/MotoControllerTest.java
@@ -0,0 +1,24 @@
+package com.betacom.veicoli.moto;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import tools.jackson.databind.ObjectMapper;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+@RequiredArgsConstructor(onConstructor_ = { @Autowired })
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MotoControllerTest {
+
+	private final MockMvc mockMvc;
+	private final ObjectMapper objectMapper;
+	
+}
